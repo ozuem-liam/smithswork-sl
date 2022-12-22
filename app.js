c9f8c374417e6ff0b0e37d16fd5823b9b2235019
Williams Onyekaozeum <Williams.Onyekaozeum@Sterling.ng>
2022-12-22 08:54:34 +0100

    sender and community endpoint

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const routes = require('./route/index');
-
-const PORT = 8080 || process.env.PORT;
+const cors = require('cors');
 
 // parse json request body
 app.use(express.json());
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(routes);
 
